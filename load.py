f1993d4a77b1cafc2c35181f43c7f29951fe320f
JovanS27 <99026645+JovanS27@users.noreply.github.com>
2022-07-04 15:13:30 +0100

    national air pollution into database

--- load.py
+++ load.py
@@ -8,10 +8,12 @@ from config import postgresPass
 from queries import gp_practice_query
 from queries import air_pollution_query
 from queries import national_gp_query
+from queries import national_air_pollution_query
 
 GP_PRACTICE_DF=pd.read_csv("gp_practice.csv")
 GP_TIME_DATA_DF=pd.read_csv("air_pollution_data.csv")
 NATIONAL_GP=pd.read_csv("national_gp.csv")
+NATIONAL_AIR_POLLUTION=pd.read_csv("national_air_pollution.csv")
 
 usr = 'postgres'
 pwd = postgresPass
@@ -29,11 +31,13 @@ print("Database exists?", database_exists(engine.url))
 engine.execute(gp_practice_query)
 engine.execute(air_pollution_query)
 engine.execute(national_gp_query)
+engine.execute(national_air_pollution_query)
 
 try:
     GP_PRACTICE_DF.to_sql(name = 'gp_practice', con = engine, if_exists='replace')
     GP_TIME_DATA_DF.to_sql(name = 'air_pollution_data', con = engine, if_exists='replace')
     NATIONAL_GP.to_sql(name = 'national_gp', con = engine, if_exists='replace')
+    NATIONAL_AIR_POLLUTION.to_sql(name = 'national_air_pollution', con = engine, if_exists='replace')
     
 except Exception as e:
     print(e)
--- queries.py
+++ queries.py
@@ -36,3 +36,42 @@ CREATE TABLE air_pollution_data (
     practice_name VARCHAR(50)
 );
 """
+
+national_gp_query = """
+DROP TABLE IF EXISTS national_gp;
+CREATE TABLE national_gp (
+    index INT PRIMARY KEY,
+    gp_practice VARCHAR(50),
+    latitude NUMERIC,
+    longitude NUMERIC,
+	asthma_percentage DECIMAL,
+    aqi DECIMAL,
+	co DECIMAL,
+	no DECIMAL,
+	no2 DECIMAL,
+	o3 DECIMAL,
+	so2 DECIMAL,
+	pm2_5 DECIMAL,
+	pm10 DECIMAL,
+	nh3 DECIMAL
+);
+"""
+
+national_air_pollution_query="""
+DROP TABLE IF EXISTS national_air_pollution;
+
+CREATE TABLE national_air_pollution (
+    index INT PRIMARY KEY,
+    date INT,
+    air_quality_index DECIMAL,
+	carbon_monoxide DECIMAL,
+	nitrogen_monoxide DECIMAL,
+	nitrogen_dioxide DECIMAL,
+	ozone DECIMAL,
+	sulphur_dioxide DECIMAL,
+	ammonia DECIMAL,
+	particultes_pm2_5 DECIMAL,
+	particulates_pm_10 DECIMAL,
+    practice_name VARCHAR(50)
+);
+"""
